7d6ba08f973b6cca62b4138a5c825a7f49d2f76e
jian308 <jian308@qq.com>
2022-02-24 12:07:45 +0800

    增加Fdir

--- conf/conf.go
+++ conf/conf.go
@@ -1,11 +1,24 @@
 package conf
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/pelletier/go-toml"
 )
 
 var cfgtoml = "conf.toml"
 var Data *toml.Tree
+
+func Fdir(f string) string {
+	fdir := f
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	_, err := os.Stat(fdir) //os.Stat获取文件信息
+	if os.IsNotExist(err) {
+		fdir = dir + "/" + f
+	}
+	return fdir
+}
 
 func Auto() {
 	Load(cfgtoml)
